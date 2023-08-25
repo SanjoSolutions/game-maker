1df5146a23bc9f7f65d7615d066677330636a61f
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-25 22:48:53 +0200

    Back to esbuild

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/generative-game' : '/',
-  build: {
-    target: ['chrome116'],
-  },
-}))
