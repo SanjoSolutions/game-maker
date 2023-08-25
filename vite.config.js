5ded9718c629a53060eb00beb3a29b2ca47c051a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-25 20:33:28 +0200

    Base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/programming' : '/',
   build: {
     target: ['chrome116'],
   },
-}
+}))
