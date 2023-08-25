dd750111ce123ac2321c12894e38a80283b1283c
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-25 20:36:45 +0200

    Fix for base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/generative-programming' : '/',
+  base: command === 'build' ? '/generative-game' : '/',
   build: {
     target: ['chrome116'],
   },
